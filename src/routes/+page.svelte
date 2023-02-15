876cb1d9f8a21f85275944c444ed9039aceb325b
santiago <santiago.borquez.1705@gmail.com>
2023-02-15 13:16:41 -0300

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<div>
+  <h1>My Notes</h1>
+  <button>Create Note</button>
+  <a href="/archives">Archived Notes</a>
+</div>
+
+<style>
+  div {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background-color: aliceblue;
+  }
+  h1 {
+    font-size: 3.5rem;
+    font-weight: 700;
+    text-transform: uppercase;
+    color: rgb(1, 29, 13);
+  }
+  button {
+    width: 7rem;
+    height: 3rem;
+    background-color: rgba(93, 104, 135);
+    color: rgb(255, 255, 255);
+    border: none;
+    border-radius: 12px;
+    padding: 0.6rem;
+    transition: ease 0.3s ;
+  }
+  button:hover {
+    background-color: rgb(71, 81, 108);
+    color: rgb(255, 255, 255);
+    cursor: pointer;
+  }
+  a {
+    text-decoration: none;
+    color: rgb(31, 9, 41);
+    font-size: 1.5rem;
+  }
+</style>
